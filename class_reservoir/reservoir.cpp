f4090a1ffe0775c47d5bbd892b8eb034875335dd
filian <filiandun@gmail.com>
2022-12-01 14:34:54 +0300

    class Reservoir (1): all constructor and destructor, get, set, is type equal, area compare

--- class_reservoir/reservoir.cpp
+++ class_reservoir/reservoir.cpp
@@ -1,1 +1,143 @@
 #include "reservoir.h"
+
+Reservoir::Reservoir()
+{
+	this->name = new char[this->name_length];
+}
+
+Reservoir::Reservoir(const char* name, short int name_length, double length, double width, double depth, short int type)
+{
+	this->name_length = name_length;
+	this->name = new char[this->name_length];
+	for (int i = 0; i < this->name_length; i++)
+	{
+		this->name[i] = name[i];
+	}
+
+	this->length = length;
+	this->width = width;
+	this->depth = depth;
+	this->type = type;
+}
+
+Reservoir::Reservoir(const Reservoir& r)
+{
+	this->name_length = r.name_length;
+	this->name = new char[this->name_length];
+	for (int i = 0; i < r.name_length; i++)
+	{
+		this->name[i] = r.name[i];
+	}
+
+	this->length = r.length;
+	this->width = r.width;
+	this->depth = r.depth;
+	this->type = r.type;
+
+	this->volume = volume;
+	this->area = area;
+}
+
+Reservoir::~Reservoir()
+{
+	delete[] this->name;
+}
+
+// SET
+void Reservoir::set_name(const char* name, short int name_length)
+{
+	this->name_length = name_length;
+	for (int i = 0; i < this->name_length; i++)
+	{
+		this->name[i] = name[i];
+	}
+}
+
+void Reservoir::set_length(long float length)
+{
+	this->length = length;
+}
+
+void Reservoir::set_width(long float width)
+{
+	this->width = width;
+}
+
+void Reservoir::set_depth(long float depth)
+{
+	this->depth = depth;
+}
+
+void Reservoir::set_type(short int type)
+{
+	this->type = type;
+}
+
+
+// GET
+void Reservoir::get_name() const
+{
+	for (int i = 0; i < this->name_length; i++)
+	{
+		std::cout << this->name[i];
+	}
+}
+
+float Reservoir::get_length() const
+{
+	return this->length;
+}
+
+float Reservoir::get_width() const
+{
+	return this->width;
+}
+
+float Reservoir::get_depth() const
+{
+	return this->depth;
+}
+
+int Reservoir::get_type() const
+{
+	return this->type;
+}
+
+float Reservoir::get_volume()
+{
+	return this->volume = length * width * depth;
+
+}
+
+float Reservoir::get_area()
+{
+	return this->area = length * width;
+}
+
+
+
+
+bool Reservoir::is_type_equal(const Reservoir& r) const
+{
+	if (this->type == r.type)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+void Reservoir::area_compare(const Reservoir& r) const
+{
+	if (this->area > r.area)
+	{
+		std::cout << "Площадь водоёма "; this->get_name(); std::cout << " больше, чем площадь водоёма "; r.get_name();
+	}
+	else if (this->area < r.area)
+	{
+		std::cout << "Площадь водоёма "; r.get_name(); std::cout << " больше, чем площадь водоёма "; this->get_name();
+	}
+	else
+	{
+		std::cout << "Площадь водоёма "; r.get_name(); std::cout << " равна площади водоёма "; this->get_name();
+	}
+}
--- class_reservoir/reservoir.h
+++ class_reservoir/reservoir.h
@@ -3,6 +3,52 @@
 
 class Reservoir
 {
+public:
+	Reservoir(); // конструктор по умолчанию
+	explicit Reservoir(const char* name, short int name_length, double length, double width, double depth, short int type); // конструктор с параметрами
+	explicit Reservoir(const Reservoir& r); // копирующий конструктор
+	~Reservoir();
 
+private:
+	short int name_length = 15;
+	char* name = nullptr;
+
+	long float length = 0; // длина
+	long float width = 0; // ширина
+	long float depth = 0; // глубина
+
+	short int type = 0; // тип 0 - бассейн, 1 - пруд, озеро, 2 - море, океан
+
+	long float volume = 0;
+	long float area = 0;
+	//long float volume = length * width * depth;
+	//long float area = length * width;
+
+public:
+	// SET
+	void set_name(const char* name, short int name_length);
+
+	void set_length(long float length);
+	void set_width(long float width);
+	void set_depth(long float depth);
+
+	void set_type(short int type);
+
+	// GET
+	void get_name() const;
+
+	float get_length() const;
+	float get_width() const;
+	float get_depth() const;
+
+	int get_type() const;
+
+	float get_volume();
+	float get_area();
+
+
+
+	bool is_type_equal(const Reservoir& r) const;
+	void area_compare(const Reservoir& r) const;
 };
 
